ea3744e31dadf2b517dc22beee688d5efae8fd95
Vesna Brekalo <VesnaNew@vesnabrkalosMBP.localdomain>
2018-07-02 14:16:34 +0100

    Add 6 TV show titles #2

--- src/App.js
+++ src/App.js
@@ -7,15 +7,27 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">All About My Mother</h1>
+            <h1 className="App-title">All About My Mother</h1>
         </header>
         <p className="App-body">
-        A Greek saying states that only women <br />
-        who have washed their eyes <br />
-        with tears can see clearly. <br />
+            A Greek saying states that only women <br />
+            who have washed their eyes <br />
+            with tears can see clearly. <br />
           <img src={image} className="AAMM" alt="logo" />
         </p>
+
+      <div className="container">
+        <h2 className="App-subtitle">Black Mirror</h2>
+        <h2 className="App-subtitle">Breaking Bad</h2>
+        <h2 className="App-subtitle">Death Note</h2>
       </div>
+      <div className="container">
+        <h2 className="App-subtitle">Game of Thrones</h2>
+        <h2 className="App-subtitle">The Walking Dead</h2>
+        <h2 className="App-subtitle">The Wire</h2>
+      </div>
+      </div>
+
     );
   }
 }
