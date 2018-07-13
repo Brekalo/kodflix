b1e0700902f410eb0171af4af3ce51c73f20ed46
Vesna Brekalo <VesnaNew@vesnabrkalosMBP.localdomain>
2018-07-13 10:26:33 +0100

    Create a Gallery component to show the covers #9

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import CoverMovie from './gallery/coverMovie';
+import CoverMovie from './frontend/gallery/CoverMovie.js';
 import './App.css';
 
 
@@ -7,9 +7,9 @@ class App extends Component {
     render() {
         return (
             <div className='App'>
-                 <div className='coverContainer'>
-                    <CoverMovie/>
-                    </div>
+                <div className='coverContainer'>
+                    <CoverMovie />
+                </div>
             </div> 
         )
     }
