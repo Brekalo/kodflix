1e321cf27a51e0757e0e32b23d8e37c0579221fe
Vesna Brekalo <VesnaNew@vesnabrkalosMBP.localdomain>
2018-08-21 17:23:29 +0100

    introduce-state  pull rquest

--- src/frontend/components/Details/Details.js
+++ src/frontend/components/Details/Details.js
@@ -3,15 +3,29 @@ import {Link} from 'react-router-dom';
 import './Details.css';
  
 class Details extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            message: 'Hello, this will be the details page for each Movie & TV show :)'
+        };
+    }
+    
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ message: 'Coming soon! :)' });
+        }, 3000);
+    }
+
     render() {
         return (
             <div id='details'>
-                <h1>Hello, this will be the details page for each Movie & TV show :)</h1> 
-                <button className='buttonBackHome' onClick={this.onNavigateHome}>
-                    <Link to={'/'} className='backHome'><h3>Home</h3></Link>
-                </button>
-            </div >
+              <h1>{this.state.message}</h1>
+              <button className='buttonBackHome' onClick={this.onNavigateHome}>
+              <Link to={'/'} className='backHome'><h3>Home</h3></Link>
+            </button>
+            </div>
         );
     }
 }
-export default Details;
+export default Details; 
