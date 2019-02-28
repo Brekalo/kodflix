6a4c090caa7874ddc74c3d73d50e12f9b81cd44b
Brekalo <vesna.brekalo@siol.net>
2019-02-28 13:12:34 +0000

    Add support for customer analytics! #35

--- src/frontend/components/App/App.js
+++ src/frontend/components/App/App.js
@@ -3,23 +3,33 @@ import CoverMovie from '../coverMovie/CoverMovie';
 import Details from '../details/Details.js';
 import './App.css';
 import Welcome from '../welcome/Welcome';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import NotFound from '../notFound/NotFound';
+import ReactGA from 'react-ga';
 
 class App extends React.Component {
+    constructor({ history, location }) {
+        super();
+        ReactGA.initialize('UA-135370186-1');
+        this.trackPageView(location);
+        history.listen((location) => this.trackPageView(location));
+    }
+
+    trackPageView(location) {
+        ReactGA.pageview(location.pathname + location.search + location.hash);
+    }
+
     render() {
         return (
-            <Router>
-                <div className='App'>
-                    <Welcome />
-                    <Switch >
-                        <Route exact path='/' component={CoverMovie} />
-                        <Route path='/notFound' component={NotFound} />
-                        <Route path='/:showId' component={Details} />
-                    </Switch>
-                </div>
-            </Router>
+            <div className='App'>
+                <Welcome />
+                <Switch >
+                    <Route exact path='/' component={CoverMovie} />
+                    <Route path='/notFound' component={NotFound} />
+                    <Route path='/:showId' component={Details} />
+                </Switch>
+            </div>
         );
     }
 }
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './frontend/components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 // import 'bootstrap/dist/css/bootstrap.css';
 
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
